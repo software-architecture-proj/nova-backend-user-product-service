6c34e82d0650da9109af117ec2d28170513f11d6
neuro-trigger <carlosandresrio33@hotmail.com>
2025-05-19 21:50:21 -0500

    fix(pocket_repo.go): correct types.

--- internal/repos/pocket_repo.go
+++ internal/repos/pocket_repo.go
@@ -3,13 +3,14 @@ package repos
 import (
     "gorm.io/gorm"
     "github.com/software-architecture-proj/nova-backend-user-product-service/internal/models"
+    "github.com/google/uuid"
 )
 
 type PocketRepository interface {
     CreatePocket(pocket *Pocket) error
-    GetPocketsByUserID(user_id string) ([]Pocket, error)
-    UpdatePocketByID(id string, new_pocket *Pocket) error
-    DeletePocketByID(id string) error
+    GetPocketsByUserID(user_id uuid.UUID) ([]Pocket, error)
+    UpdatePocket(pocket *Pocket) error
+    DeletePocketByID(id uuid.UUID) error
 }
 
 type pocketRepo struct {
@@ -24,18 +25,18 @@ func (r *pocketRepo) CreatePocket(pocket *Pocket) error {
 	return r.db.Create(pocket).Error
 }
 
-func (r *pocketRepo) GetPocketsByUserID(user_id string) ([]Pocket, error) {
+func (r *pocketRepo) GetPocketsByUserID(user_id uuid.UUID) ([]Pocket, error) {
 	var pockets []Pocket
-	if err := r.db.Where("User_ID = ?", user_id).Find(&pockets).Error; err != nil {
+	if err := r.db.Where("user_id = ?", user_id).Find(&pockets).Error; err != nil {
 		return nil, err
 	}
 	return pockets, nil
 }
 
-func (r *pocketRepo) UpdatePocketByID(id string, new_pocket *Pocket) error {
-    return r.db.Model(&Pocket{}).Where("ID = ?", id).Updates(new_pocket).Error
+func (r *pocketRepo) UpdatePocket(pocket *Pocket) error {
+    return r.db.Model(&Pocket{}).Where("id = ?", Pocket.ID).Updates(pocket).Error
 }
 
-func (r *pocketRepo) DeletePocketByID(id string) error {
-    return r.db.Where("ID = ?", id).Delete(&Pocket{}).Error
+func (r *pocketRepo) DeletePocketByID(id uuid.UUID) error {
+    return r.db.Where("id = ?", id).Delete(&Pocket{}).Error
 }
