3d971797bf3a38bf8213d673620639b136e6fef6
neuro-trigger <carlosandresrio33@hotmail.com>
2025-05-19 20:58:29 -0500

    feature(user_repo.go): complete repo for user CRUD management.

--- internal/repos/user_repo.go
+++ internal/repos/user_repo.go
@@ -7,8 +7,10 @@ import (
 
 type UserRepository interface {
 	CreateUser(user *User) error
-	GetUserByID(id uint) (*User, error)
-	ListUsers() ([]User, error)
+    GetUserByID(id uuid.UUID) (*User, error)
+	UpdateUser(user *User) error
+	DeleteUser(id uuid.UUID) error
+    ListUsers() ([]User, error)
 }
 
 type userRepo struct {
@@ -23,7 +25,15 @@ func (r *userRepo) CreateUser(user *User) error {
 	return r.db.Create(user).Error
 }
 
-func (r *userRepo) GetUserByID(id uint) (*User, error) {
+func (r *userRepo) UpdateUser(user *User) error {
+    return r.db.Model(&User{}).Where("id = ?", user.ID).Updates(user).Error
+}
+
+func (r *userRepo) DeleteUserById(id uuid.UUID) error {
+    return r.db.Delete(&models.User{}, "id = ?", id).Error
+}
+
+func (r *userRepo) GetUserByID(id uuid.UUID) (*User, error) {
 	var user User
 	if err := r.db.First(&user, id).Error; err != nil {
 		return nil, err
